d1003eb8db47d29e45c413ae702f2ccd8e8fc5c6
UIMSolutions <ui.manufaktur@gmail.com>
2023-12-05 07:31:28 +0100

    Update

--- source/web/angular/module_.d
+++ source/web/angular/module_.d
@@ -3,13 +3,13 @@ module uim.angular.module_;
 class DNgModule {
     this() {}
 
-    private string[string] _controllers;
-    @property string[string] controllers() { return _controllers; }
-    @property O controllers(this O)(string[string] newControllers) { _controllers = newControllers; return cast(O)this; }
+    private STRINGAA _controllers;
+    @property STRINGAA controllers() { return _controllers; }
+    @property O controllers(this O)(STRINGAA newControllers) { _controllers = newControllers; return cast(O)this; }
 
-    private string[string] _directives;
-    @property string[string] directives() { return _directives; }
-    @property O directives(this O)(string[string] newDirectives) { _directives = newDirectives; return cast(O)this; }
+    private STRINGAA _directives;
+    @property STRINGAA directives() { return _directives; }
+    @property O directives(this O)(STRINGAA newDirectives) { _directives = newDirectives; return cast(O)this; }
 
     bool opEqual(string txt) { return (toString == txt); }
     override string toString() { return ""; }
